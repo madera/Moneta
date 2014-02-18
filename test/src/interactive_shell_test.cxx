5ec6d9fb4942243273493a8ec1d9d2848e606a64
Rodrigo Madera <rodrigo.madera@gmail.com>
2014-02-17 21:51:13 -0300

    Commiting externals... more of a test, really...

--- test/src/interactive_shell_test.cxx
+++ test/src/interactive_shell_test.cxx
@@ -5,28 +5,6 @@
 
 #include <console/shell.hxx>
 
-template <class Shell, class Input, class Output>
-void loop_until_quit_or_exit(Shell& shell, Input& input, Output& output) {
-	bool done = false;
-	while (!done) {
-		output << shell.prompt();
-
-		std::string line;
-		std::getline(input, line);
-
-		try {
-			shell(line);
-		}
-		catch (std::exception& ex) {
-			std::cerr << "TOP EXCEPTION: " << ex.what() << std::endl;
-		}
-
-		if (line == "exit" || line == "quit") {
-			done = true;
-		}
-	}
-}
-
 BOOST_AUTO_TEST_CASE(interactive_shell_test) {
 	console::shell<> shell;
 
@@ -34,5 +12,5 @@ BOOST_AUTO_TEST_CASE(interactive_shell_test) {
 		std::cerr << "Hello, motherfucker!" << std::endl;
 	});
 
-	loop_until_quit_or_exit(shell, std::cin, std::cout);
+	console::loop_until_quit_or_exit(shell, std::cin, std::cout);
 }
