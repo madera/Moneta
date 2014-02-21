80e33c848ae243c47b023585f49efbb68dbfd104
Rodrigo Madera <rodrigo.madera@gmail.com>
2014-02-20 22:45:13 -0300

    More migration steps, like meta.

--- build/msvc/Tests/stdafx.h
+++ build/msvc/Tests/stdafx.h
@@ -1,15 +1,13 @@
-// stdafx.h : include file for standard system include files,
-// or project specific include files that are used frequently, but
-// are changed infrequently
-//
-
 #pragma once
-
 #include "targetver.h"
 
-#include <stdio.h>
-#include <tchar.h>
+//
+// Boost
+//
+#include <boost/test/unit_test.hpp>
+#include <boost/test/unit_test_suite.hpp>
 
-
-
-// TODO: reference additional headers your program requires here
+//
+// Std
+//
+#include <iostream>
