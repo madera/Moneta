3f0bd9fbd75c0e8daa40b4616484e85caab5c68c
Rodrigo Madera <rodrigo.madera@gmail.com>
2014-03-30 01:47:10 -0300

    Refactored: Renamed db_tuple to rtuple

---
+++ include/moneta/sql/traits/rtuple.hxx
@@ -0,0 +1,15 @@
+#pragma once
+#include "../../traits/pk.hxx"
+#include "../../traits/tuple.hxx"
+
+namespace moneta { namespace sql { namespace traits {
+
+	template <class EntityType>
+	struct rtuple : boost::fusion::result_of::as_vector<
+		typename boost::mpl::transform<
+			typename moneta::traits::detail::mpl::vector<EntityType>::type,
+			moneta::traits::pk<boost::mpl::_1>
+		>::type
+	> {};
+
+}}}
