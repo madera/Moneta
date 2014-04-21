8d1cc03185a557d97641b71471ccbf1dba6f2b86
Rodrigo Madera <rodrigo.madera@gmail.com>
2014-04-21 14:01:57 -0300

    Mid commit

--- test/moneta/container/rcontext_test.cxx
+++ test/moneta/container/rcontext_test.cxx
@@ -1,7 +1,9 @@
 #include "stdafx.h"
 #include <moneta/container/rcontext.hxx>
 #include <boost/mpl/equal.hpp>
+#include <boost/foreach.hpp>
 #include "../model/Cat.hxx"
+#include "../model/CatDataset.hxx"
 #include "../model/Dog.hxx"
 #include "../model/Composite.hxx"
 
@@ -38,47 +40,8 @@ BOOST_AUTO_TEST_CASE(rcontext_test) {
 	BOOST_CHECK_EQUAL(context.size<Cat>(), 0);
 	BOOST_CHECK_EQUAL(context.size<Address>(), 0);
 
-	{
-		Cat cat;
-		cat.ID = 1;
-		cat.Name = "Somecat";
-		cat.Address.ID = 10;
-		cat.Address.Number = 10;
-		cat.Address.Street = "Ten St.";
-
-		context.insert(cat);
-	}
-
-	{
-		Cat cat;
-		cat.ID = 2;
-		cat.Name = "Garfield";
-		cat.Address.ID = 20;
-		cat.Address.Number = 20;
-		cat.Address.Street = "Twenty St.";
-
-		context.insert(cat);
-	}
-
-	{
-		Cat cat;
-		cat.ID = 3;
-		cat.Name = "Felix";
-		cat.Address.ID = 30;
-		cat.Address.Number = 30;
-		cat.Address.Street = "Thirty St.";
-
-		context.insert(cat);
-	}
-
-	{
-		Cat cat;
-		cat.ID = 4;
-		cat.Name = "Bob";
-		cat.Address.ID = 40;
-		cat.Address.Number = 40;
-		cat.Address.Street = "Forty St.";
-
+	std::vector<Cat> dataset = test_dataset<Cat>()();
+	BOOST_FOREACH(Cat cat, dataset) {
 		context.insert(cat);
 	}
 
