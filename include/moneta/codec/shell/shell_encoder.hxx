997bb7e56a101357fb29394c3f22ca37268fc46b
Rodrigo Madera <rodrigo.madera@gmail.com>
2015-01-19 00:01:15 -0200

    Migrated shell_encoder

--- include/moneta/codec/shell/shell_encoder.hxx
+++ include/moneta/codec/shell/shell_encoder.hxx
@@ -1,72 +1,74 @@
 #pragma once
 #include "../../traits/entity_name.hxx"
-#include "../encoder.hxx"
+#include "../../traits/member_names.hxx"
+#include "../encoder_ng.hxx"
 #include "../codec_io.hxx"
 
-namespace moneta { namespace codec {
+namespace moneta { namespace codec { namespace shell_implementation {
 
- 	struct shell;
-
-	template <class Path, class Entity>
-	struct enter_entity_encoder<shell, Path, Entity, typename boost::enable_if<
-		boost::mpl::empty<Path>
-	>::type> {
-		template <class Iterator>
-		int operator()(const Entity& entity, Iterator begin, Iterator end) const {
+	struct enter_entity_encoder {
+		template <class Iterator, class Entity, class Path>
+		typename boost::enable_if<
+			boost::mpl::empty<Path>,
+			int
+		>::type
+		operator()(Iterator begin, Iterator end, const Entity& entity, const Path&) const {
 			return moneta::codec::detail::make_ostringstream(begin, end)
 				<< moneta::traits::get_entity_name<Entity>() << "={"
 			;
 		}
-	};
 
-	template <class Path, class Entity>
-	struct enter_entity_encoder<shell, Path, Entity, typename boost::disable_if<
-		boost::mpl::empty<Path>
-	>::type> {
-		template <class Iterator>
-		int operator()(const Entity& entity, Iterator begin, Iterator end) const {
+		template <class Iterator, class Entity, class Path>
+		typename boost::disable_if<
+			boost::mpl::empty<Path>,
+			int
+		>::type
+		operator()(Iterator begin, Iterator end, const Entity& entity, const Path&) const {
 			return moneta::codec::detail::make_ostringstream(begin, end)
 				<< ' ' << moneta::traits::get_entity_name<Entity>() << "={"
 			;
 		}
 	};
 
-	//
-
-	template <class Member, class Path>
-	struct member_encoder<shell, Member, Path, typename boost::disable_if<
-		boost::is_same<typename Member::result_type, std::string>
-	>::type> {
-		template <class Entity, class Iterator>
-		int operator()(const Entity& entity, Iterator begin, Iterator end) const {
+	struct member_encoder {
+		template <class Iterator, class Member, class Entity>
+		typename boost::disable_if<
+			boost::is_same<typename Member::result_type, std::string>, int
+		>::type
+		operator()(Iterator begin, Iterator end, const Member&, const Entity& entity) const {
 			return moneta::codec::detail::make_ostringstream(begin, end)
 				<< ' ' << traits::detail::member_name<Member>::get() << '=' << Member()(entity)
 			;
 		}
-	};
 
-	template <class Member, class Path>
-	struct member_encoder<shell, Member, Path, typename boost::enable_if<
-		boost::is_same<typename Member::result_type, std::string>
-	>::type> {
-		template <class Entity, class Iterator>
-		int operator()(const Entity& entity, Iterator begin, Iterator end) const {
+		template <class Iterator, class Member, class Entity>
+		typename boost::enable_if<
+			boost::is_same<typename Member::result_type, std::string>, int
+		>::type
+		operator()(Iterator begin, Iterator end, const Member&, const Entity& entity) const {
 			return moneta::codec::detail::make_ostringstream(begin, end)
 				<< ' ' << traits::detail::member_name<Member>::get() << "=\"" << Member()(entity) << '"'
 			;
 		}
 	};
 
-	//
-
-	template <class Path, class Entity>
-	struct leave_entity_encoder<shell, Path, Entity> {
-		template <class Iterator>
-		int operator()(const Entity& entity, Iterator begin, Iterator end) const {
+	struct leave_entity_encoder {
+		template <class Iterator, class Entity>
+		int operator()(Iterator begin, Iterator end, const Entity& entity) const {
 			return moneta::codec::detail::make_ostringstream(begin, end)
 				<< " }"
 			;
 		}
 	};
 
+	typedef moneta::codec::encoder<
+		enter_actions <enter_entity_encoder>,
+		member_actions<member_encoder>,
+		leave_actions <leave_entity_encoder>
+	> shell_encoder;
+
+}}}
+
+namespace moneta { namespace codec {
+	using shell_implementation::shell_encoder;
 }}
--- tests/core/moneta/codec/shell/shell_encoder_test.cxx
+++ tests/core/moneta/codec/shell/shell_encoder_test.cxx
@@ -20,30 +20,26 @@ static Composite make_composite() {
 	return composite;
 }
 
-BOOST_AUTO_TEST_CASE(shell_encoder_test_1) {
-	const Composite composite = make_composite();
-	
+BOOST_AUTO_TEST_CASE(shell_encoder_test_1) {	
 	char buffer[1024];
-	memset(buffer, 0, sizeof(buffer));
-
-	moneta::codec::encode<moneta::codec::shell>(composite, buffer, buffer + sizeof(buffer));
+	std::fill(std::begin(buffer), std::end(buffer), 0);
 
 	const std::string expected =
 		"Composite={ Identifier=2600 Person={ ID=123 Name=\"Somedude\" Height=1.5 Fingers=10 } "
 		"Dog={ Owner=\"Someowner\" ID=555 Name=\"Doggy\" } }"
 	;
 
-	BOOST_CHECK_EQUAL(expected, buffer);
+	moneta::codec::shell_encoder()(std::begin(buffer), std::end(buffer), make_composite());
+	BOOST_CHECK_EQUAL(buffer, expected);
 }
 
 
 BOOST_AUTO_TEST_CASE(shell_encoder_test_2) {
-
 	char buffer[1024];
-	memset(buffer, 0, sizeof(buffer));
-
-	moneta::codec::encode<moneta::codec::shell>(moneta::make_entity<A>(), buffer, buffer + sizeof(buffer));
+	std::fill(std::begin(buffer), std::end(buffer), 0);
 
 	const std::string expected = "A={ f=0 g=0 B={ C={ j=0 k=0 } i=0 D={ l=0 E={ m=0 n=0 } } } h=0 }";
-	BOOST_CHECK_EQUAL(expected, buffer);
+
+	moneta::codec::shell_encoder()(std::begin(buffer), std::end(buffer), moneta::make_entity<A>());
+	BOOST_CHECK_EQUAL(buffer, expected);
 }
