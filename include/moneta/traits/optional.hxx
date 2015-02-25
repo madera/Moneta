ede3955855a11cd7ccf94b014a509465905a0aef
Rodrigo Madera <rodrigo.madera@gmail.com>
2015-02-25 00:11:35 -0300

    Corrections to traits::optional

--- include/moneta/traits/optional.hxx
+++ include/moneta/traits/optional.hxx
@@ -1,3 +1,4 @@
+#pragma once
 #include <boost/optional.hpp>
 
 namespace moneta { namespace traits {
@@ -8,12 +9,81 @@ namespace moneta { namespace traits {
 	template <typename T>
 	struct is_optional<boost::optional<T> > : boost::true_type {};
 
+	template <typename T>
+	struct is_optional<boost::optional<T>&> : boost::true_type {};
+
+	template <typename T>
+	struct is_optional<const boost::optional<T> > : boost::true_type {};
+
+	template <typename T>
+	struct is_optional<const boost::optional<T>&> : boost::true_type {};
+
 	//
 
-	template <class T>
+	template <class T, class Enable = void>
 	struct optional_value_type : boost::mpl::identity<T> {};
 
 	template <class T>
-	struct optional_value_type<boost::optional<T> > : boost::mpl::identity<T> {};
+	struct optional_value_type<
+		T,
+		typename boost::enable_if<
+			is_optional<T>
+		>::type
+	> : boost::mpl::if_<
+		boost::is_const<T>,
+		typename boost::add_const<typename T::value_type>::type,
+		typename T::value_type
+	> {};
+
+	//
+
+	namespace detail {
+		template <typename T, typename Enable = void>
+		struct get_optional_value_impl {
+			T& operator()(T& x) const {
+				return x;
+			}
+		};
+
+		template <typename T>
+		struct get_optional_value_impl<
+			T,
+			typename boost::enable_if<
+				is_optional<T>
+			>::type
+		> {
+			typename optional_value_type<T>::type& operator()(T& x) {
+				return x.get();
+			}
+		};
+	}
+
+	template <typename T>
+	typename optional_value_type<T>::type& get_optional_value(T& x) {
+		return detail::get_optional_value_impl<T>()(x);
+	}
+
+	//
+
+	namespace detail {
+		template <typename T, typename Enable = void>
+		struct is_optional_present_impl {
+			T& operator()(T& x) const {
+				return false;
+			}
+		};
+
+		template <typename T>
+		struct is_optional_present_impl<boost::optional<T> > {
+			bool operator()(boost::optional<T>& x) const {
+				return x.is_initialized();
+			}
+		};
+	}
+
+	template <typename T>
+	bool is_optional_present(T& x) {
+		return detail::is_optional_present_impl<T>()(x);
+	}
 
 }}
